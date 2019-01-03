9a362bf2649ae5a3a577f1f6ede8713f975c9cdd
Stephen Wright <scw248@gmail.com>
2019-01-03 02:23:16 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,6 +13,8 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter += 1
   if counter == number_of_times
+    break
+  end
 end
 
 def times_iterator(number_of_times)
