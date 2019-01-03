20d249ef7154afe33774cf468180b1ef1da0f353
Stephen Wright <scw248@gmail.com>
2019-01-03 02:29:26 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -34,6 +34,8 @@ def while_iterator(number_of_times)
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  
+  
 end
 
 def until_iterator(number_of_times)
